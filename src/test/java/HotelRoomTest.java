9d7a4920524507da97c0c0adbd4ee47069d19ddb
rksgr <r_kjha@yahoo.com>
2022-01-09 08:31:58 +0530

    Refactored: Find cheapest hotel for given date range take weekday and weekend into account

--- src/test/java/HotelRoomTest.java
+++ src/test/java/HotelRoomTest.java
@@ -3,6 +3,7 @@ import service.HotelBook;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -29,5 +30,23 @@ public class HotelRoomTest {
         hotelbook.addWeekdayWeekendRates();
         Integer room_rate = hotelbook.getHotelReservationSystem().get(0).getRoomRateWeekday();
         Assertions.assertEquals(110,room_rate);
+    }
+    @Test
+    public void givenDateRange_WhenSearchCheapestHotels_ShouldReturnCheapstHotel(){
+        // Add three hotels
+        hotelbook.addHotel(new Hotel("Lakewood",110,90));
+        hotelbook.addHotel(new Hotel("Bridgewood",150,50));
+        hotelbook.addHotel(new Hotel("Ridgewood",220,150));
+
+        // Dates for which cheap hotel has to be searched
+        LocalDate localDate1 = LocalDate.of(2020,9,11);
+        LocalDate localDate2 = LocalDate.of(2020,9,12);
+        ArrayList<String[]> chp_hotels = hotelbook.findCheapHotelWeekDayOrEnd(localDate1, localDate2);
+
+        // Assert total rates and hotel names
+        assertEquals("200",chp_hotels.get(0)[1]);
+        assertEquals("200",chp_hotels.get(1)[1]);
+        assertEquals("Lakewood",chp_hotels.get(0)[0]);
+        assertEquals("Bridgewood",chp_hotels.get(1)[0]);
     }
 }
