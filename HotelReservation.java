949c52e5976a52bf32ea8920d087fe1de457c3de
rksgr <r_kjha@yahoo.com>
2022-01-10 12:56:55 +0530

    Refactored: Cheapest best rated hotel for a reward customer

--- HotelReservation.java
+++ HotelReservation.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDate;
+
 public class HotelReservation {
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel reservation System!");
+        LocalDate localDate = LocalDate.of(2001,12,11);
+        LocalDate localDate1 = LocalDate.of(2020,9,10);
+        LocalDate localDate2 = LocalDate.of(2020,9,11);
     }
 }
